3ca148aa9ed3c5b0dd1605a59dd7aa57f8365893
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2026-09-15 15:17:20 +0100

    CN-204-update-rm-events-to-match-event-dictionary (#22)

--- src/main/java/uk/gov/ons/census/notifysvc/utils/Constants.java
+++ src/main/java/uk/gov/ons/census/notifysvc/utils/Constants.java
@@ -3,9 +3,8 @@ package uk.gov.ons.census.notifysvc.utils;
 import java.util.Set;
 
 public class Constants {
-  public static final String OUTBOUND_EVENT_SCHEMA_VERSION = "0.5.0";
-  public static final Set<String> ALLOWED_INBOUND_EVENT_SCHEMA_VERSIONS =
-      Set.of("v0.3_RELEASE", "0.4.0-DRAFT", "0.4.0", "0.5.0-DRAFT", "0.5.0", "0.6.0-DRAFT");
+  public static final String OUTBOUND_EVENT_SCHEMA_VERSION = "1.0.0";
+  public static final Set<String> ALLOWED_INBOUND_EVENT_SCHEMA_VERSIONS = Set.of("1.0.0");
   public static final String TEMPLATE_UAC_KEY = "__uac__";
   public static final String TEMPLATE_QID_KEY = "__qid__";
   public static final String TEMPLATE_CASEREF_KEY = "__caseref__";
